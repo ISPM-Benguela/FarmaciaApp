a1a9ce3ae4579905e28bbdb8310498d958c0d5b1
tandavala <jose.tandavala@gmail.com>
2019-11-24 07:07:50 +0100

    criando o controlador perfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,12 +56,17 @@
       </div>
       <div class="top-menu">
         <ul class="nav pull-right top-menu">
+          <li class="nav-item">
+            <a href="" class="logout">Meu Perfil</a>
+          </li>
           <li class="nav-item dropdown">
             <a id="navbarDropdown" class="logout dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                 {{ Auth::user()->name }} <span class="caret"></span>
             </a>
-
+            
             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+             
+                
                 <a class="dropdown-item" href="{{ route('logout') }}"
                    onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
