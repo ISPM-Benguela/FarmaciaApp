e222c276d5655a33964b0f7ad13ea062b982ab3a
tandavala <jose.tandavala@gmail.com>
2019-11-26 07:17:15 +0100

    migrando a tabela carrinho

---
+++ database/migrations/2019_11_26_061417_create_carrinhos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrinhosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carrinhos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('produto_id')->unsigned();
+            $table->integer('quantidade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carrinhos');
+    }
+}
